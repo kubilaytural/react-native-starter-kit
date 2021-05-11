541a24f6982322f05d9d3be922b9588266b23b50
kubilaytural <kubilay.tural@asnak.com>
2021-05-11 12:45:36 +0300

    Add eslint, babel, prettier config files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [require.resolve('babel-plugin-module-resolver'), {
+      root: ['./src'],
+      extensions: ['.js', '.ios.js', '.android.js'],
+    }],
+  ],
+  env: {
+    production: {
+      plugins: ["transform-remove-console"],
+    },
+  },
 };
